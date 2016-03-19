85f3280cc913a0540fa88537daef1e8c5b7ee704
imran.shabbbir@gmail.com <imran.shabbbir@gmail.com>
2016-03-20 00:31:05 +0500

    commit

--- ServeUpApiServer/Startup.cs
+++ ServeUpApiServer/Startup.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Net.Http.Extensions.Compression.Core.Compressors;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
+using Microsoft.AspNet.WebApi.Extensions.Compression.Server;
 using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.DataHandler.Encoder;
 using Microsoft.Owin.Security.Jwt;
@@ -18,13 +20,15 @@ namespace ServeUpApiServer
         // This method is required by Katana:
         public void Configuration(IAppBuilder app)
         {
-
-            ConfigureOAuthTokenConsumption(app);
             var webApiConfiguration = new HttpConfiguration();
+            app.Use<LoggingMiddleware>(app);
+            ConfigureOAuthTokenConsumption(app);            
+            
             ConfigureWebApi(webApiConfiguration);
-            ConfigureWebApiDocumentation(webApiConfiguration);
+            ConfigureWebApiDocumentation(webApiConfiguration);            
             // Use the extension method provided by the WebApi.Owin library:                       
             app.UseWebApi(webApiConfiguration);
+            webApiConfiguration.MessageHandlers.Insert(0, new ServerCompressionHandler(new GZipCompressor(), new DeflateCompressor()));
         }
 
         private void ConfigureWebApi(HttpConfiguration config)
